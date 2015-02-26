04d875164b8751a2457fa4a52b533aceda82e312
jasonjchang <hrjasonchang@gmail.com>
2015-02-26 10:45:55 -0800

    Accepts urls from client, routes correctly

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-var header = require('http-helpers');
+var header = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
@@ -8,14 +8,25 @@ exports.handleRequest = function (req, res) {
     res.writeHead(200, header.headers);
     res.end('get');
     console.log('get working');
-  } else if(req.method === 'OPTIONS') {
+  } else
+  if(req.method === 'OPTIONS') {
     res.writeHead(200, header.headers);
     res.end('options');
     console.log('OPTIONS WORKING');
-  } else if(req.method === 'POST') {
+  } else
+  if(req.method === 'POST') {
     res.writeHead(201, header.headers);
-    res.end('post');
-    console.log('POST WORKING');
+    var urlObj = '';
+    req.on('data', function(data) {
+      urlObj += data;
+    });
+    req.on('end', function(){
+      urlObj = JSON.parse(urlObj);
+      // console.log(urlObj, urlObj.url )
+      //eventually handle www issue, as well as lowercase
+      archive.isURLArchived(urlObj.url.toLowerCase());
+    });
+    res.end('POST complete');
   }
 };
 
